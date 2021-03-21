76a76b6e7596064605678bf29eedb0ce8b0bc044
Ana <ana.gamez204360@potros.itson.edu.mx>
2021-03-21 00:39:08 -0700

    clases: Recordatorio y Carrito, XML Recordatorio

---
+++ app/src/main/java/gamez/ana/mydigimind/Carrito.kt
@@ -0,0 +1,12 @@
+package gamez.ana.mydigimind
+
+import java.io.Serializable
+
+class Carrito : Serializable {
+
+    var recordatorios = ArrayList<Recordatorio>()
+
+    fun agregar(p: Recordatorio): Boolean {
+        return recordatorios.add(p)
+    }
+}
